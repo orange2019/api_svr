9d1318fd0ed8f86e1515a22af6c3b0ebdeb9823a
lucong <332553882@qq.com>
2018-12-03 11:02:02 +0800

    数据库配置

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ module.exports = {
   db: {
     host: '59939c0a9a983.gz.cdb.myqcloud.com',
     port: 5579,
-    dbname: '2018_kaximu_test',
+    dbname: '2018_kxm_dev',
     username: 'kaximu',
     password: 'kaximu2018',
     maxLimit: 100,
--- config/test.js
+++ config/test.js
@@ -2,5 +2,15 @@ module.exports = {
   domain: {
     img1: 'http://img1.kxm.cc512.com',
     img2: 'http://img2.kxm.cc512.com'
-  }
+  },
+
+  db: {
+    host: '59939c0a9a983.gz.cdb.myqcloud.com',
+    port: 5579,
+    dbname: '2018_kaximu_test',
+    username: 'kaximu',
+    password: 'kaximu2018',
+    maxLimit: 1000,
+  },
+
 }
