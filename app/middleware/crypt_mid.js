6bca4193cc7f7a849ef7d80df97988ba901e80a3
lucong <332553882@qq.com>
2018-12-06 18:43:16 +0800

    ad

--- app/middleware/crypt_mid.js
+++ app/middleware/crypt_mid.js
@@ -19,8 +19,8 @@ module.exports = async (req, res, next) => {
       })
     }
 
-    Log.info(`${uuid}|${req.originalUrl}|req.content`, JSON.stringify(content))
-    let cryptStr = cryptUtil.hmacMd5(JSON.stringify(content), uuid)
+    Log.info(`${uuid}|${req.originalUrl}|req.content`, content)
+    let cryptStr = cryptUtil.hmacMd5(content, uuid)
     Log.info(`${uuid}|${req.originalUrl}|req.cryptStr`, cryptStr)
     // 验证签名
     let verify = cryptUtil.verify(cryptStr, sign, publicKey)
