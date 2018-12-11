49a3f5739658cccd5a48593c177d88da8549d81e
lucong <332553882@qq.com>
2018-12-11 20:26:48 +0800

    ad

--- testRpc.js
+++ testRpc.js
@@ -1,5 +1,5 @@
 var TestRPC = require("ethereumjs-testrpc")
 var server = TestRPC.server()
-server.listen(8550, function (err, blockchain) {
+server.listen(8545, function (err, blockchain) {
   console.log(blockchain)
 })
