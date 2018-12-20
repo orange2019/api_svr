9da27151f5be1bf5a9e9004050080c5f9fa42486
lucong <332553882@qq.com>
2018-12-20 10:25:35 +0800

    ad

--- app/controller/admin/auth.js
+++ app/controller/admin/auth.js
@@ -2,20 +2,25 @@ const express = require('express')
 const router = express.Router()
 const adminService = require('./../../service/admin_service')
 
-router.post('/login' , async(req, res) => {
+router.post('/login', async (req, res) => {
 
   await adminService.login(req.ctx)
   return res.return(req.ctx)
-  
+
 })
 
-router.post('/check' , async(req, res) => {
+router.post('/check', async (req, res) => {
   await adminService.check(req.ctx)
   return res.return(req.ctx)
 })
 
-router.post('/logout' , async(req, res) => {
+router.post('/logout', async (req, res) => {
   await adminService.logout(req.ctx)
+  return res.return(req.ctx)
+})
+
+router.post('/adminUpdate', async (req, res) => {
+  await adminService.update(req.ctx)
   return res.return(req.ctx)
 })
 
--- app/service/admin_service.js
+++ app/service/admin_service.js
@@ -12,29 +12,39 @@ class AdminService {
   async login(ctx) {
 
     let ret = {
-      code : errCode.SUCCESS.code,
-      message : errCode.SUCCESS.message
+      code: errCode.SUCCESS.code,
+      message: errCode.SUCCESS.message
     }
-    let { email, password } = ctx.body
+    let {
+      email,
+      password,
+      captcha
+    } = ctx.body
+    if (captcha != ctx.session.captcha) {
+      ret.code = errCode.FAIL.code
+      ret.message = '验证码错误'
 
+      ctx.result = ret
+      return ret
+    }
     let admin = await adminModel().model().findOne({
       where: {
         email: email
       }
     })
-    Log.info(`${ctx.uuid}|login().admin` , admin , cryptoUtils.md5(password))
+    Log.info(`${ctx.uuid}|login().admin`, admin, cryptoUtils.md5(password))
 
-    if(!admin || admin.password.toUpperCase() != cryptoUtils.md5(password)){
+    if (!admin || admin.password.toUpperCase() != cryptoUtils.md5(password)) {
       ret.code = errCode.FAIL.code
       ret.message = '登录失败'
       // return ret
-    }else {
+    } else {
       ctx.session.admin = {
         id: admin.id,
         email: admin.email,
-        type : admin.type
+        type: admin.type
       }
-      
+
     }
 
     ctx.result = ret
@@ -46,29 +56,75 @@ class AdminService {
    * 检查登录状态
    * @param {*} ctx 
    */
-  async check(ctx){
+  async check(ctx) {
     let ret = {
-      code : errCode.SUCCESS.code,
-      message : errCode.SUCCESS.message
+      code: errCode.SUCCESS.code,
+      message: errCode.SUCCESS.message
     }
 
     let session = ctx.session
     let admin = session.admin || null
-    if(!admin || !admin.id) {
+    if (!admin || !admin.id) {
       ret.code = errCode.ADMIN.authFail.code
       ret.message = errCode.ADMIN.authFail.message
-    }else {
+    } else {
       ret.data = admin
     }
-    
+
     ctx.result = ret
     return ret
   }
 
+  async update(ctx) {
+    let ret = {
+      code: errCode.SUCCESS.code,
+      message: errCode.SUCCESS.message
+    }
+
+    let {
+      password_old,
+      password,
+      password_again
+    } = ctx.body
+
+    let session = ctx.session
+    let admin = session.admin || null
+    if (!admin || !admin.id) {
+      ret.code = errCode.ADMIN.authFail.code
+      ret.message = errCode.ADMIN.authFail.message
+
+      ctx.result = ret
+      return ret
+    }
+    if (password != password_again) {
+      ret.code = errCode.ADMIN.authFail.code
+      ret.message = '两次输入密码不一致'
+
+      ctx.result = ret
+      return ret
+    }
+
+    let adminInfo = await adminModel().model().findById(admin.id)
+    if (adminInfo.password.toUpperCase() != cryptoUtils.md5(password_old)) {
+      ret.code = errCode.ADMIN.authFail.code
+      ret.message = '旧密码错误'
+
+      ctx.result = ret
+      return ret
+    }
+
+    adminInfo.password = cryptoUtils.md5(password)
+    await adminInfo.save()
+    ctx.result = ret
+    return ret
+
+
+  }
+
   async logout(ctx) {
     let ret = {
-      code : errCode.SUCCESS.code,
-      message : errCode.SUCCESS.message
+      code: errCode.SUCCESS.code,
+      message: errCode.SUCCESS.message
     }
 
     ctx.session.admin = null
