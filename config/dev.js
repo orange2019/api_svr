1148dcadfb2c9ff24160db356bd063e42568840a
lucong <332553882@qq.com>
2018-12-22 16:16:28 +0800

    配置没改回来

--- config/dev.js
+++ config/dev.js
@@ -1,13 +1,13 @@
 module.exports = {
 
-  // db: {
-  //   host: '59939c0a9a983.gz.cdb.myqcloud.com',
-  //   port: 5579,
-  //   dbname: '2018_kxm_dev',
-  //   username: 'kaximu',
-  //   password: 'kaximu2018',
-  //   maxLimit: 1000,
-  // },
+  db: {
+    host: '59939c0a9a983.gz.cdb.myqcloud.com',
+    port: 5579,
+    dbname: '2018_kxm_dev',
+    username: 'kaximu',
+    password: 'kaximu2018',
+    maxLimit: 1000,
+  },
 
-  // default_contract_id: 2,
+  default_contract_id: 2,
 }
