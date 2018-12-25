490c225127634884a052c3b3128a15c780d72289
lucong <332553882@qq.com>
2018-12-25 19:58:53 +0800

    ad

--- update/wallet.js
+++ update/wallet.js
@@ -1,6 +1,6 @@
 module.exports = {
   latestVersion: '1.0.0',
-  latestVersionCode: 2,
+  latestVersionCode: 3,
   url: 'http://kxm-img.oss-cn-shenzhen.aliyuncs.com/app/Download/kxm_wallet.apk',
   releaseNotes: [
     '1.0.1',
