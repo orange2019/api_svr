4ee5050727eba93b276906a03ef9a50294fee205
lucong <332553882@qq.com>
2018-12-26 17:02:08 +0800

    update

--- update/wallet.js
+++ update/wallet.js
@@ -1,8 +1,9 @@
 module.exports = {
-  latestVersion: '1.0.0',
-  latestVersionCode: 3,
+  latestVersion: '1.0.3',
+  latestVersionCode: 4,
   url: 'http://kxm-img.oss-cn-shenzhen.aliyuncs.com/app/Download/mck_wallet.apk',
   releaseNotes: [
+    '修复实名认证bug',
     '1.0.1',
     '测试版1.0.0',
     '1.0.0',
