31d9e6414bb8e5d40c71935d7223c327b6c53c75
lucong <332553882@qq.com>
2019-01-02 11:03:21 +0800

    ad

--- update/dapp.js
+++ update/dapp.js
@@ -1,8 +1,9 @@
 module.exports = {
   latestVersion: '1.0.0',
-  latestVersionCode: 1,
+  latestVersionCode: 2,
   url: 'http://kxm-img.oss-cn-shenzhen.aliyuncs.com/app/Download/mck_dapp.apk',
   releaseNotes: [
-    '1.0.0'
+    '1.0.1'
+    // '1.0.0'
   ]
 }
