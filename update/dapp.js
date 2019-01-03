61ca7ba09580e26cc712e50aac323a524826e02f
lucong <332553882@qq.com>
2019-01-03 09:50:17 +0800

    ad

--- update/dapp.js
+++ update/dapp.js
@@ -1,6 +1,6 @@
 module.exports = {
   latestVersion: '1.0.0',
-  latestVersionCode: 4,
+  latestVersionCode: 3,
   url: 'http://kxm-img.oss-cn-shenzhen.aliyuncs.com/app/Download/mck_dapp.apk',
   releaseNotes: [
     '1.0.2:tab切换去掉'
