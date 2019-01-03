49d8a94099dddf72138011cd4103a30299387dab
lucong <332553882@qq.com>
2019-01-03 14:52:17 +0800

    update

--- update/wallet.js
+++ update/wallet.js
@@ -1,10 +1,11 @@
 module.exports = {
-  latestVersion: '1.0.4',
-  latestVersionCode: 5,
+  latestVersion: '1.0.5',
+  latestVersionCode: 6,
   url: 'http://kxm-img.oss-cn-shenzhen.aliyuncs.com/app/Download/mck_wallet.apk',
   releaseNotes: [
-    '1.修复进入实名认证闪退',
-    '2.优化上传身份证组件'
+    '闪退问题'
+    // '1.修复进入实名认证闪退',
+    // '2.优化上传身份证组件'
     // '修复实名认证bug',
     // '1.0.1',
     // '测试版1.0.0',
